78c688f88b481a3ff75b5d990f9ca68ac98529bc
Ryan Dale <115406+daler@users.noreply.github.com>
2025-10-10 15:34:35 -0400

    pass additional args to ./run-playbook

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 [ -z $TF_VAR_EC2_LOGIN_KEY ] && echo 'Missing $TF_VAR_EC2_LOGIN_KEY (.pem file or SSH key prefix)' && exit 1
-ansible-playbook playbook.yaml -i hosts --private-key $TF_VAR_EC2_LOGIN_KEY -u ubuntu
+ansible-playbook playbook.yaml -i hosts --private-key $TF_VAR_EC2_LOGIN_KEY -u ubuntu "$@"
