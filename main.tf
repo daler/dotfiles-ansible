cd85ee450205d5de083b148d4217c817a4110343
Ryan Dale <115406+daler@users.noreply.github.com>
2025-10-17 16:09:12 -0400

    address terraform notification for python version

--- main.tf
+++ main.tf
@@ -212,7 +212,7 @@ resource "aws_lambda_function" "check_instance" {
   function_name = "check-instance-uptime"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
-  runtime       = "python3.9"
+  runtime       = "python3.10"
   timeout       = 30
   source_code_hash = filebase64sha256("lambda_function.zip")
 
